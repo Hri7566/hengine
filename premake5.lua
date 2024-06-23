fb4d8aaac8c12643a916a462624ee02c795b2e3a
Hri7566 <hri7566@gmail.com>
2024-06-23 00:06:27 -0400

    Update gitignore and add premake-ninja

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+-- premake5 build file
+-- https://premake.github.io/
+
+---@diagnostic disable
+-- stylua: ignore start
+
+workspace "Hello"
+	configurations { "Debug", "Release" }
+
+project "HelloWorld"
+	require "premake-ninja.ninja"
+	kind "ConsoleApp"
+	language "C"
+	targetdir "bin/%{cfg.buildcfg}"
+
+	files { "src/**.hpp", "src/**.cpp" }
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+
+-- stylua: ignore end
